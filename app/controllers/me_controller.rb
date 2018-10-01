24efef37297afe3894b72a182d8d04bb29abf2ad
johnmcbryan <johnmcbryan5@gmail.com>
2018-10-01 00:44:20 -0400

    5_User_Features:
    -Ranktables you have made are being displayed in profile page
    -Ranktables you have voted on is being displayed in profile page

--- app/controllers/me_controller.rb
+++ app/controllers/me_controller.rb
@@ -1,6 +1,10 @@
 class MeController < ApplicationController
   before_action :authenticate
+  # TODO Make sure the votes table is displaying the right tables in the user profile page
   def show
+    @ranktables = Ranktable.where(authorID: current_user.id)
+    @tableIDs = Vote.where(userID: current_user.id).select(:tableID)
+    @votetables = Ranktable.where(id: @tableIDs)
 
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+  provider :google_oauth2, '191813446617-9ngq3s8c8lmu8f3fc18gksufgv84qvru.apps.googleusercontent.com', 'THEkXQXFE4ei76aElO4C8oTX'
 end
