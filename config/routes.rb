c1cdd3132273dbd2c1faf760f7f8c8e8c128a389
johnmcbryan <johnmcbryan5@gmail.com>
2018-09-30 22:45:36 -0400

    4_Add_Users:
    -Oauth is working with user data being saved correctly
    -User sessions are being created
    -Login is working
    -Need to display user data and verify login upon moving around the website

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "login#index"
+  root "home#show"
 
   resources :ranktables do
     resources :votes
@@ -8,7 +8,14 @@ Rails.application.routes.draw do
 
   resources :votes
 
-  resources :login
+  get 'login', to: redirect('/auth/google_oauth2'), as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'home', to: 'home#show'
+  get 'me', to: 'me#show', as: 'me'
+
+  root to: "home#show"
 
   get "/register", to: redirect("/auth/google_oauth2")
   get "/auth/google_oauth2/callback", to: redirect("/ranktables")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_30_051752) do
+ActiveRecord::Schema.define(version: 2018_10_01_021240) do
 
   create_table "ranktables", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 2018_09_30_051752) do
     t.integer "authorID"
     t.integer "count"
     t.text "options"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
